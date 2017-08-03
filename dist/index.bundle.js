36bfd8862c78daffa09881263d5b8b02660f8212
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-08-03 14:06:13 -0400

    add dist files to repo

---
+++ dist/index.bundle.js
@@ -0,0 +1,144 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _index = __webpack_require__(1);
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function imageUploads() {
+	var featuredImageWrap = document.querySelectorAll('.custom-media-upload');
+
+	if (featuredImageWrap.length === 0) {
+		return;
+	}
+
+	[].map.call(featuredImageWrap, function (item) {
+		// var ImageWrapId = item.attribute.id;
+		var removeImage = item.querySelector('.remove-image');
+		var replaceImage = item.querySelector('.replace-image');
+		var setImage = item.querySelector('.set-image');
+		var featuredImageTag = item.querySelector('a.replace-image img');
+		var featuredImageID = item.querySelector('input[type="hidden"]');
+		var mediaFrame = wp.media({
+			title: 'Choose Media',
+			button: {
+				text: 'Use Selected Media'
+			},
+			multiple: false
+		});
+
+		mediaFrame.on('select', function () {
+			var attachment = mediaFrame.state().get('selection').first().toJSON();
+
+			item.classList.toggle('has-image');
+			featuredImageID.value = attachment.id;
+			if (attachment.hasOwnProperty('sizes')) {
+				featuredImageTag.setAttribute('src', attachment.sizes.thumbnail.url);
+			} else {
+				featuredImageTag.setAttribute('src', attachment.icon);
+			}
+		});
+
+		removeImage.addEventListener('click', function (event) {
+			event.preventDefault();
+			item.classList.toggle('has-image');
+			featuredImageTag.setAttribute('src', '');
+			featuredImageID.val('');
+		});
+
+		replaceImage.addEventListener('click', function (event) {
+			event.preventDefault();
+			mediaFrame.open();
+		});
+
+		setImage.addEventListener('click', function (event) {
+			event.preventDefault();
+			mediaFrame.open();
+		});
+	});
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	imageUploads();
+});
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=index.bundle.js.map
