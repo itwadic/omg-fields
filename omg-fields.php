ac39840bd61d146034f8dd4e7cd2d9d006f92baf
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-09-20 14:55:05 -0400

    run js build and bump version to 0.4.0

--- omg-fields.php
+++ omg-fields.php
@@ -13,7 +13,7 @@ if ( !defined( 'OMG_FIELDS_FILE' ) ) {
 }
 
 if ( !defined( 'OMG_FIELDS_VERSION' ) ) {
-	define( 'OMG_FIELDS_VERSION', '0.3.0' );
+	define( 'OMG_FIELDS_VERSION', '0.4.0' );
 }
 
 require_once __DIR__ . '/core/autoload.php';
