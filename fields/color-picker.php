fc6ab8fc05a4c2793719c73c55d7a131d2a49643
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-10-08 14:09:43 -0400

    normalize color-picker field api to match the other field types

--- fields/color-picker.php
+++ fields/color-picker.php
@@ -1,7 +1,7 @@
 <?php
 namespace OMG\Fields;
 
-function register_color_picker( $post, $name, $label, $default_color = '#10aded', $option ) {
+function register_color_picker( $post, $name, $label, $default_color = '#10aded', $args = [] ) {
 	$value = get_post_meta( $post->ID, $name, true );
 	$value = empty( $value ) ? $default_color : $value;
 	ob_start();
@@ -11,8 +11,8 @@ function register_color_picker( $post, $name, $label, $default_color = '#10aded'
 			<?php esc_html_e( $label ) ?>
 		</label>
 		<div class="row-wrapper">
-			<?php if ( isset( $args['description'] ) && ! empty( $option['description'] ) ) : ?>
-                <p class="admin-row-description"><?php echo esc_html( $option['description'] ); ?></p>
+			<?php if ( isset( $args['description'] ) && ! empty( $args['description'] ) ) : ?>
+                <p class="admin-row-description"><?php echo esc_html( $args['description'] ); ?></p>
 			<?php endif; ?>
 			<input type="text" name="<?php echo esc_attr( $name ); ?>" value="<?php echo esc_attr( $value ); ?>" class="color-field" />
 			<script>
