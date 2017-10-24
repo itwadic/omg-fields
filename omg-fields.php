6df7e7c8194110898dfde1e6da8695dfedbfdc6c
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-10-24 15:59:23 -0400

    bump version

--- omg-fields.php
+++ omg-fields.php
@@ -3,25 +3,13 @@
 if ( !defined( 'OMG_FIELDS_DIR' ) ) {
 	define( 'OMG_FIELDS_DIR', dirname( __FILE__ ) );
 }
-//
-//if ( !defined( 'OMG_FIELDS_URL' ) ) {
-//	$locate = locate_template( '/vendor/omg-fields/omg-fields.php' );
-//	if ( ! empty( $locate ) ) {
-//		define( 'OMG_FIELDS_URL', get_stylesheet_directory_uri() . '/vendor' );
-//		define( 'OMG_FIELDS_CONTEXT', 'wordpress-theme' );
-//	} else {
-//		define( 'OMG_FIELDS_URL', plugin_dir_url( __FILE__ ) );
-//		define( 'OMG_FIELDS_CONTEXT', 'wordpress-plugin' );
-//	}
-//
-//}
 
 if ( !defined( 'OMG_FIELDS_FILE' ) ) {
 	define( 'OMG_FIELDS_FILE', __FILE__ );
 }
 
 if ( !defined( 'OMG_FIELDS_VERSION' ) ) {
-	define( 'OMG_FIELDS_VERSION', '0.6.0' );
+	define( 'OMG_FIELDS_VERSION', '0.7.0' );
 }
 
 require_once __DIR__ . '/core/autoload.php';
