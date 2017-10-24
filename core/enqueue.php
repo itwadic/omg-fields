50dbd41d7a3ca5afe4ad18dc78c966166b825a57
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-10-24 15:50:49 -0400

    update library to work in both themes and plugins

--- core/enqueue.php
+++ core/enqueue.php
@@ -1,13 +1,16 @@
 <?php
 namespace OMG\Fields\Core;
 
+use OMG\Fields\Directory;
+
 function setup() {
   add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\scripts' );
   add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\styles' );
 }
 
 function scripts() {
-  wp_enqueue_script( 'omg-fields-js', OMG_FIELDS_URL . '/omg-fields/dist/index.bundle.js', array(), OMG_FIELDS_VERSION, true );
+
+	wp_enqueue_script( 'omg-fields-js', Directory\local_url( '/dist/index.bundle.js', __DIR__ ), array(), OMG_FIELDS_VERSION, true );
 
 	wp_localize_script(
 		'omg-fields-js',
@@ -19,9 +22,9 @@ function scripts() {
 }
 
 function styles() {
-  wp_enqueue_style(
+	wp_enqueue_style(
 		'omg-fields-css',
-		OMG_FIELDS_URL . '/omg-fields/dist/index.bundle.css',
+		Directory\local_url( '/dist/index.bundle.css', __DIR__ ),
 		array(),
 		OMG_FIELDS_VERSION
 	);
--- omg-fields.php
+++ omg-fields.php
@@ -3,10 +3,18 @@
 if ( !defined( 'OMG_FIELDS_DIR' ) ) {
 	define( 'OMG_FIELDS_DIR', dirname( __FILE__ ) );
 }
-
-if ( !defined( 'OMG_FIELDS_URL' ) ) {
-	define( 'OMG_FIELDS_URL', get_stylesheet_directory_uri() . '/vendor' );
-}
+//
+//if ( !defined( 'OMG_FIELDS_URL' ) ) {
+//	$locate = locate_template( '/vendor/omg-fields/omg-fields.php' );
+//	if ( ! empty( $locate ) ) {
+//		define( 'OMG_FIELDS_URL', get_stylesheet_directory_uri() . '/vendor' );
+//		define( 'OMG_FIELDS_CONTEXT', 'wordpress-theme' );
+//	} else {
+//		define( 'OMG_FIELDS_URL', plugin_dir_url( __FILE__ ) );
+//		define( 'OMG_FIELDS_CONTEXT', 'wordpress-plugin' );
+//	}
+//
+//}
 
 if ( !defined( 'OMG_FIELDS_FILE' ) ) {
 	define( 'OMG_FIELDS_FILE', __FILE__ );
