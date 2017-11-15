b3a538871501ccd7d9a3793d3f1c8b4e89addd41
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-15 14:14:31 -0500

    bump version to 0.9.0

--- omg-fields.php
+++ omg-fields.php
@@ -9,7 +9,7 @@ if ( !defined( 'OMG_FIELDS_FILE' ) ) {
 }
 
 if ( !defined( 'OMG_FIELDS_VERSION' ) ) {
-	define( 'OMG_FIELDS_VERSION', '0.8.0' );
+	define( 'OMG_FIELDS_VERSION', '0.9.0' );
 }
 
 require_once __DIR__ . '/core/autoload.php';
