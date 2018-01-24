112f97329243c811b8b626001387f0cbedc42e9c
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2018-01-24 14:26:42 -0500

    Format with prettier, update getValue function to return empty array if empty

--- assets/js/serialized-input.js
+++ assets/js/serialized-input.js
@@ -1,27 +1,28 @@
-export default function( parent, hiddenInput ) {
-    const hiddenEl = parent.querySelector(hiddenInput);
+export default function(parent, hiddenInput) {
+  const hiddenEl = parent.querySelector(hiddenInput);
 
-    const getCurrentValue = () => JSON.parse( hiddenEl.value );
+  const getCurrentValue = () =>
+    hiddenEl.value ? JSON.parse(hiddenEl.value) : [];
 
-    const addValue = (value) => {
-        const currentValue = getCurrentValue();
-        return currentValue.concat( value );
-    }
+  const addValue = value => {
+    const currentValue = getCurrentValue();
+    return currentValue.concat(value);
+  };
 
-    const removeValue = (value, onRemove) => {
-        const currentValue = getCurrentValue();
-        return onRemove( currentValue, value );
-    }
+  const removeValue = (value, onRemove) => {
+    const currentValue = getCurrentValue();
+    return onRemove(currentValue, value);
+  };
 
-    return {
-        add: function(value) {
-            hiddenEl.value = JSON.stringify(addValue(value));
-        },
-        remove: function(value, onRemove) {
-            hiddenEl.value = JSON.stringify(removeValue(value, onRemove));
-        },
-        update: function(values) {
-            hiddenEl.value = JSON.stringify(values);
-        }
-    }
+  return {
+    add: function(value) {
+      hiddenEl.value = JSON.stringify(addValue(value));
+    },
+    remove: function(value, onRemove) {
+      hiddenEl.value = JSON.stringify(removeValue(value, onRemove));
+    },
+    update: function(values) {
+      hiddenEl.value = JSON.stringify(values);
+    },
+  };
 }
